509270c256f8c1cf31019865ec8a9dfa02a3d7da
bhawna-thoughtwin <bhawna@thoughtwin.com>
2025-04-22 11:17:03 +0530

    add routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/bhawna-thoughtwin/Task1.git
+// git push -u origin main
---
+++ src/pages/Blog.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>Blog page</div>
+  )
+}
+export default Blog;
